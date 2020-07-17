83db3e1ceebde334a2eec71e3a1e6fcf5af4d12c
tgulmine <tgulmine@gmail.com>
2020-07-17 06:57:09 -0300

    remove warning

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { BrowserRouter as Route, Link } from 'react-router-dom';
 
 interface HeaderProps {
